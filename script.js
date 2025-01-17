c9487976b5284920851e8fb9f295055d4a6b0cde
Shivank Singh <shivank12309@gmail.com>
2025-01-17 12:20:21 +0530

    Implement logic for choice

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Suprabhat")
+function GetComputerChoice(){
+    return Math.floor(Math.random() * 3);
+}
+let p=GetComputerChoice();
+function GetHumanChoice(){
+    let userChoice=parseInt(prompt("Enter your choice"));
+    console.log(userChoice);
+}
+GetHumanChoice();
